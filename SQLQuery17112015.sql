a69c01bfde6bf6695543277dcc8a9f22bb19e879
andreluizsombra <andreluizsombra@gmail.com>
2016-03-29 18:52:42 -0300

    novo arq

---
+++ SQLQuery17112015.sql
@@ -0,0 +1,8 @@
+select prc_credor as X_CREDOR from notaempenho where NOTAEMP=21030001 and ORGAO=07 and UNDORC=02
+
+select (select prc_credor as X_CREDOR from notaempenho where NOTAEMP=PRC_NOTAEMP and ORGAO=PRC_ORGAO and UNDORC=PRC_UND_ORC)
+,* from processos where prc_notaemp != 'EXTRA' and PRC_CREDOR is null
+
+select * from PROCESSOS where PRC_DTREFDOC=022014 and prc_notaemp != 'EXTRA' and PRC_CREDOR is null
+
+select * from NOTAEMPENHO
