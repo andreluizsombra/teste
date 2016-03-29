9d927e45803063a4de8f60102ff06dfa5f064ebc
andreluizsombra <andreluizsombra@gmail.com>
2016-03-29 18:54:39 -0300

    Segundo commit

--- SQLQuery17112015.sql
+++ SQLQuery17112015.sql
@@ -1,8 +1,9 @@
+-- Andre Sombra
 select prc_credor as X_CREDOR from notaempenho where NOTAEMP=21030001 and ORGAO=07 and UNDORC=02
 
 select (select prc_credor as X_CREDOR from notaempenho where NOTAEMP=PRC_NOTAEMP and ORGAO=PRC_ORGAO and UNDORC=PRC_UND_ORC)
 ,* from processos where prc_notaemp != 'EXTRA' and PRC_CREDOR is null
 
-select * from PROCESSOS where PRC_DTREFDOC=022014 and prc_notaemp != 'EXTRA' and PRC_CREDOR is null
+select * from PROCESSOS where PRC_DTREFDOC=012016 and prc_notaemp != 'EXTRA' and PRC_CREDOR is null
 
-select * from NOTAEMPENHO
+select * from NOTAEMPENHO --Teste comentario
