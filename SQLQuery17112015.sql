408d382dc6e70d3ce48949c7d9d5097b4ce93052
andresombra <andresombra.net@gmail.com>
2016-10-04 15:11:28 -0300

    Atualizei data sqlscript

--- SQLQuery17112015.sql
+++ SQLQuery17112015.sql
@@ -1,4 +1,6 @@
 -- Andre Sombra
+--Atualizado em 04/10/2016
+
 select prc_credor as X_CREDOR from notaempenho where NOTAEMP=21030001 and ORGAO=07 and UNDORC=02
 
 select (select prc_credor as X_CREDOR from notaempenho where NOTAEMP=PRC_NOTAEMP and ORGAO=PRC_ORGAO and UNDORC=PRC_UND_ORC)
